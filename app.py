640bade70bbc7f9262f418528e0a0ea6b4d66eef
Krishnabh-Das <116840384+Krishnabh-Das@users.noreply.github.com>
2023-08-25 02:32:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, jsonify
-import importlib
+import nltk
 import re
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled
 
 app = Flask(__name__)
+
+def summarize_text(text):
+    sentences = nltk.sent_tokenize(text)
+    summary = sentences[0]
+    for sentence in sentences[1:]:
+        summary += ' ' + sentence
+    summary = summary[:500] + '...' if len(summary) > 500 else summary
+    return summary
 
 def clean_text(text):
     # Remove extra spaces and spaces before punctuation
@@ -29,32 +37,11 @@ def get_summary_from_link(link):
         for i in transcript:
             result += i['text']+' '
 
-        
-        # Import sumy.parsers.plaintext
-        PlaintextParser = importlib.import_module("sumy.parsers.plaintext").PlaintextParser
-        
-        # Import sumy.nlp.tokenizers
-        Tokenizer = importlib.import_module("sumy.nlp.tokenizers").Tokenizer
-        
-        # Import sumy.summarizers.lsa
-        LsaSummarizer = importlib.import_module("sumy.summarizers.lsa").LsaSummarizer
-        
-        # Your long text
-        long_text = """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida nulla nec dui ultrices, vitae tincidunt purus accumsan. Donec eu justo tortor. Ut nec diam eget urna fringilla cursus. Proin ullamcorper leo at tellus bibendum luctus. Nullam sit amet facilisis libero. Sed nec libero vel justo malesuada eleifend. Suspendisse euismod libero a odio faucibus, id euismod quam scelerisque. In hac habitasse platea dictumst. Quisque condimentum lectus vel odio bibendum, a interdum sapien finibus. Integer fringilla vitae leo sit amet fermentum.
-        """
-        
-        # Initialize the parser and tokenizer
-        parser = PlaintextParser.from_string(long_text, Tokenizer("english"))
-        
-        # Initialize the summarizer
-        summarizer = LsaSummarizer()
-        
-        # Get the summary
-        summary = summarizer(parser.document, sentences_count=3)  # Adjust the number of sentences as needed
-        
+        summary = summarize_text(result)
+
         cleaned_out = remove_bracketed_words(summary)  # Remove words and brackets
         cleaned_out = clean_text(cleaned_out)
+        
         return cleaned_out
 
     except TranscriptsDisabled as e:
