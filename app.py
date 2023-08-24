dffb03d6b08e95e30c5dac61423dc13eea823bd1
Krishnabh-Das <116840384+Krishnabh-Das@users.noreply.github.com>
2023-08-25 01:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ from youtube_transcript_api._errors import TranscriptsDisabled
 
 app = Flask(__name__)
 
-def get_summary_from_link(link, value):
+def clean_text(text):
+    # Remove extra spaces and spaces before punctuation
+    cleaned_text = re.sub(r'\s+', ' ', text)
+    cleaned_text = re.sub(r'\s([.,;!?])', r'\1', cleaned_text)
+    return cleaned_text.strip()
+
+def remove_bracketed_words(text):
+    # Remove words enclosed in square brackets including the brackets
+    cleaned_text = re.sub(r'\[.*?\]', '', text)
+    return cleaned_text
+
+def get_summary_from_link(link):
     try:
         if '=' in link:
             video_id = link.split("=")[1]
@@ -13,9 +24,12 @@ def get_summary_from_link(link, value):
             video_id = link.split("e/")[1]
 
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        result = " ".join([i['text'] for i in transcript])
-
-        return result
+        result = ""
+        for i in transcript:
+            result += ' ' + i['text']
+        cleaned_out = remove_bracketed_words(result)  # Remove words and brackets
+        cleaned_out = clean_text(cleaned_out)
+        return cleaned_out
 
     except TranscriptsDisabled as e:
         return "Transcripts are disabled for this video."
